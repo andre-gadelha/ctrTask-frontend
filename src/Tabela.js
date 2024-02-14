0b448685ad492f6ab348613776cad819a9a1cb0f
André Gadelha <adrrda@gmail.com>
2024-02-14 11:55:01 -0300

    Commit inicial

---
+++ src/Tabela.js
@@ -0,0 +1,28 @@
+function Tabela(){
+
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Prioridade</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </tbody>
+
+        </table>
+    )
+
+}
+
+export default Tabela;
